5dda893c7edef11c1bde716110bb49764fcd612d
Kshitij Gajapure <kshitijiitgn@gmail.com>
2018-10-28 01:30:31 +0530

    Added Unity files

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour 
+{
+	public Text scoreText;
+
+	private void Start()
+	{
+		scoreText.text = PlayerPrefs.GetInt ("score").ToString();
+	}
+		
+	public void ToGame ()
+	{
+		SceneManager.LoadScene("Game");
+	}
+}
